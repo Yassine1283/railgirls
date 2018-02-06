4951d04c1d31273937b05784d323d458668e553f
seddaoui.yassine@cra.education <seddaoui.yassine@cra.education>
2018-02-06 18:05:11 -0500

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'pages/info'
-
-  root to: redirect('/ideas')
+  root :to => 'ideas#index'
   resources :ideas
+  get 'pages/info'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
